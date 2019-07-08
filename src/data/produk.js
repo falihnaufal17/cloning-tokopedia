077be7294c2d1923ae08eae3c8c7685b9967d037
falihnaufal17 <dudu.kotkot@gmail.com>
2019-07-08 21:34:59 +0700

    display dropdown kategory

---
+++ src/data/produk.js
@@ -0,0 +1,93 @@
+const data = [
+    {
+        "nama_produk": "SENTER AJAIB PEMBESAR APA SAJA",
+        "harga": 1200100,
+        "foto": "https://cdn.idntimes.com/content-images/post/20161208/5-35b6343f68c36d03c2495f7c84e1dede.jpg",
+        "nama_kategori": "elektronik",
+        "NamaPenjual": "koh lim si penjual elektronik",
+        "telpon": 657474627,
+        "email": "koh@lim.com"
+    },
+    {
+        "nama_produk": "JET PACK GTA SA",
+        "harga": 90900900,
+        "foto": "http://1.bp.blogspot.com/-K02ax-jdvIQ/TmLiCTXKWCI/AAAAAAAAAFU/SoB93uWJ8W0/s1600/jetpack+grand+theft+auto+san+andreas",
+        "nama_kategori": "elektronik",
+        "NamaPenjual": "koh lim si penjual elektronik",
+        "telpon": 657474627,
+        "email": "koh@lim.com"
+    },
+    {
+        "nama_produk": "POPOK PEMBESAR BAYI",
+        "harga": 99000,
+        "foto": "https://my-best.id/wp-content/uploads/2018/01/Keunggulan-dan-kekurangan-dari-popok-tipe-tape-300x212.jpg",
+        "nama_kategori": "ibu dan bayi",
+        "NamaPenjual": "bu siti si penjual alat bayi",
+        "telpon": 123456789,
+        "email": "siti@markonah.com"
+    },
+    {
+        "nama_produk": "DOT SUSU EMAK",
+        "harga": 11000,
+        "foto": "https://i0.wp.com/asibayi.com/wp-content/uploads/2011/08/Mimijumi-Botol-Susu-Mirip-Payudara.jpg",
+        "nama_kategori": "ibu dan bayi",
+        "NamaPenjual": "bu siti si penjual alat bayi",
+        "telpon": 123456789,
+        "email": "siti@markonah.com"
+    },
+    {
+        "nama_produk": "BAN SEREP",
+        "harga": 121233,
+        "foto": "https://cdn0-production-images-kly.akamaized.net/C1SSK5Z5MWSo85Y6mLjhAlKZwGg=/640x360/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2242313/original/001072900_1528358502-Ban_serep.jpg",
+        "nama_kategori": "otomotif",
+        "NamaPenjual": "alexander parto si penjual otomotif",
+        "telpon": 696969696,
+        "email": "alex@parto.com"
+    },
+    {
+        "nama_produk": "AVANZA GRANDMAX",
+        "harga": 222222,
+        "foto": "https://ik.imagekit.io/hj8sm3kk7/large/gallery/color/38/1654/toyota-avanza-2019-color-463819.jpg",
+        "nama_kategori": "otomotif",
+        "NamaPenjual": "alexander parto si penjual otomotif",
+        "telpon": 696969696,
+        "email": "alex@parto.com"
+    },
+    {
+        "nama_produk": "RAKET NYAMUK",
+        "harga": 111111,
+        "foto": "https://1.bp.blogspot.com/-hLFvhXrcLWM/WpnrtvXHimI/AAAAAAAANqQ/byNK1gywi1wQ-cIBSAKUVpndqDTSO8VNACK4BGAYYCw/s320/tips-menggunakan-raket-nyamuk-789571.jpg",
+        "nama_kategori": "olahraga",
+        "NamaPenjual": "bu kayn si penjual alat olahraga",
+        "telpon": 123123123,
+        "email": "tolong@bukain.com"
+    },
+    {
+        "nama_produk": "BOLA DRIBEL",
+        "harga": 44444,
+        "foto": "https://s.kaskus.id/images/2017/05/05/9672704_201705051203500795.jpg",
+        "nama_kategori": "olahraga",
+        "NamaPenjual": "bu kayn si penjual alat olahraga",
+        "telpon": 123123123,
+        "email": "tolong@bukain.com"
+    },
+    {
+        "nama_produk": "LEMARI IKEA",
+        "harga": 567654,
+        "foto": "https://ecs7.tokopedia.net/img/cache/700/product-1/2019/1/11/4482427/4482427_016a1e90-1f8b-494b-beb9-2789890347af_700_700.jpg",
+        "nama_kategori": "furniture",
+        "NamaPenjual": "mang jaja si penjual furniture",
+        "telpon": 987654321,
+        "email": "jaja@miharja.com"
+    },
+    {
+        "nama_produk": "KASUR RUSAK",
+        "harga": 998990,
+        "foto": "https://ecs7.tokopedia.net/img/product-1/2017/5/26/184809604/184809604_d0d1782e-f6a0-44e1-b808-f5eae5be4f8f_1024_755.jpg",
+        "nama_kategori": "furniture",
+        "NamaPenjual": "mang jaja si penjual furniture",
+        "telpon": 987654321,
+        "email": "jaja@miharja.com"
+    }
+]
+module.exports = data
