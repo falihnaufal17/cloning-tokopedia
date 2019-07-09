5a5b90886a6307525b792ed4a0f870cc8fdd39fa
falihnaufal17 <dudu.kotkot@gmail.com>
2019-07-09 17:09:19 +0700

    detail book by id

--- src/pages/detailProduct.js
+++ src/pages/detailProduct.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
+import dummy from '../data/produk'
 export default class DetailProduct extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: dummy
+        }
+    }
     render() {
+        let getId = Number(this.props.match.params.id_product)
+        let dataById = dummy.find((item => item.id_produk === getId))
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1>{dataById.id_produk}. {dataById.product_name}</h1>
             </div>
         )
     }
