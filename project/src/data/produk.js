45e0de945db347944e024404fe3a5f13fa2180d7
abdilahtop <abdilahtop@gmail.com>
2019-07-09 05:17:27 -0700

    home

---
+++ project/src/data/produk.js
@@ -0,0 +1,103 @@
+const data = [
+    {
+        "product_name": "SENTER AJAIB PEMBESAR APA SAJA",
+        "price": 1200100,
+        "image": "https://cdn.idntimes.com/content-images/post/20161208/5-35b6343f68c36d03c2495f7c84e1dede.jpg",
+        "category_name": "elektronik",
+        "seller_name": "koh lim si penjual elektronik",
+        "phone": 657474627,
+        "email": "koh@lim.com",
+        "location": "jepang"
+    },
+    {
+        "product_name": "JET PACK GTA SA",
+        "price": 90900900,
+        "image": "http://1.bp.blogspot.com/-K02ax-jdvIQ/TmLiCTXKWCI/AAAAAAAAAFU/SoB93uWJ8W0/s1600/jetpack+grand+theft+auto+san+andreas",
+        "category_name": "elektronik",
+        "seller_name": "koh lim si penjual elektronik",
+        "phone": 657474627,
+        "email": "koh@lim.com",
+        "location": "san andreas"
+    },
+    {
+        "product_name": "POPOK PEMBESAR BAYI",
+        "price": 99000,
+        "image": "https://my-best.id/wp-content/uploads/2018/01/Keunggulan-dan-kekurangan-dari-popok-tipe-tape-300x212.jpg",
+        "category_name": "ibu dan bayi",
+        "seller_name": "bu siti si penjual alat bayi",
+        "phone": 123456789,
+        "email": "siti@markonah.com",
+        "location": "los santos"
+    },
+    {
+        "product_name": "DOT SUSU EMAK",
+        "price": 11000,
+        "image": "https://i0.wp.com/asibayi.com/wp-content/uploads/2011/08/Mimijumi-Botol-Susu-Mirip-Payudara.jpg",
+        "category_name": "ibu dan bayi",
+        "seller_name": "bu siti si penjual alat bayi",
+        "phone": 123456789,
+        "email": "siti@markonah.com",
+        "location": "purba lingga"
+    },
+    {
+        "product_name": "BAN SEREP",
+        "price": 121233,
+        "image": "https://cdn0-production-images-kly.akamaized.net/C1SSK5Z5MWSo85Y6mLjhAlKZwGg=/640x360/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2242313/original/001072900_1528358502-Ban_serep.jpg",
+        "category_name": "otomotif",
+        "seller_name": "alexander parto si penjual otomotif",
+        "phone": 696969696,
+        "email": "alex@parto.com",
+        "location": "europe"
+    },
+    {
+        "product_name": "AVANZA GRANDMAX",
+        "price": 222222,
+        "image": "https://ik.imagekit.io/hj8sm3kk7/large/gallery/color/38/1654/toyota-avanza-2019-color-463819.jpg",
+        "category_name": "otomotif",
+        "seller_name": "alexander parto si penjual otomotif",
+        "phone": 696969696,
+        "email": "alex@parto.com",
+        "location": "lenteng agung"
+    },
+    {
+        "product_name": "RAKET NYAMUK",
+        "price": 111111,
+        "image": "https://1.bp.blogspot.com/-hLFvhXrcLWM/WpnrtvXHimI/AAAAAAAANqQ/byNK1gywi1wQ-cIBSAKUVpndqDTSO8VNACK4BGAYYCw/s320/tips-menggunakan-raket-nyamuk-789571.jpg",
+        "category_name": "olahraga",
+        "seller_name": "bu kayn si penjual alat olahraga",
+        "phone": 123123123,
+        "email": "tolong@bukain.com",
+        "location": "srilangka"
+    },
+    {
+        "product_name": "BOLA DRIBEL",
+        "price": 44444,
+        "image": "https://s.kaskus.id/images/2017/05/05/9672704_201705051203500795.jpg",
+        "category_name": "olahraga",
+        "seller_name": "bu kayn si penjual alat olahraga",
+        "phone": 123123123,
+        "email": "tolong@bukain.com",
+        "location": "myanmar"
+    },
+    {
+        "product_name": "LEMARI IKEA",
+        "price": 567654,
+        "image": "https://ecs7.tokopedia.net/img/cache/700/product-1/2019/1/11/4482427/4482427_016a1e90-1f8b-494b-beb9-2789890347af_700_700.jpg",
+        "category_name": "furniture",
+        "seller_name": "mang jaja si penjual furniture",
+        "phone": 987654321,
+        "email": "jaja@miharja.com",
+        "location": "papua new guinea"
+    },
+    {
+        "product_name": "KASUR RUSAK",
+        "price": 998990,
+        "image": "https://ecs7.tokopedia.net/img/product-1/2017/5/26/184809604/184809604_d0d1782e-f6a0-44e1-b808-f5eae5be4f8f_1024_755.jpg",
+        "category_name": "furniture",
+        "seller_name": "mang jaja si penjual furniture",
+        "phone": 987654321,
+        "email": "jaja@miharja.com",
+        "location": "hawai"
+    }
+]
+module.exports = data
