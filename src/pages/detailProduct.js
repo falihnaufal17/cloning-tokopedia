4656b0281e2942004af94103bbcb40cfe63f2678
falihnaufal17 <dudu.kotkot@gmail.com>
2019-07-09 21:23:23 +0700

    add sweetalert2

--- src/pages/detailProduct.js
+++ src/pages/detailProduct.js
@@ -6,15 +6,43 @@ export default class DetailProduct extends Component {
         super(props)
 
         this.state = {
-            data: dummy
+            data: dummy,
+            cart: [],
+            qty: 0,
+
         }
+
+        this.add = this.add.bind(this)
+        this.substract = this.substract.bind(this)
+        this.addToCart = this.addToCart.bind(this)
     }
+
+    add() {
+        this.setState({
+            qty: this.state.qty + 1
+        })
+    }
+
+    substract() {
+        this.setState({
+            qty: this.state.qty - 1
+        })
+    }
+
+    addToCart(qtys) {
+        
+        console.log("Add to cart success")
+    }
+
     render() {
         let getId = Number(this.props.match.params.id_product)
         let dataById = dummy.find((item => item.id_produk === getId))
         return (
-            <div>
+            <div key={dataById.id_produk}>
                 <h1>{dataById.id_produk}. {dataById.product_name}</h1>
+                <button disabled={this.state.qty < 1} onClick={this.substract}>-</button> Qty: {this.state.qty} <button onClick={this.add}>+</button>
+                <p>{dataById.price * this.state.qty}</p>
+                <button onClick={this.addToCart}>Tambah ke keranjang</button>
             </div>
         )
     }
