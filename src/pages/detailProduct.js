36a9771dc99d024cc2aaf4475bb210260b1535eb
falihnaufal17 <dudu.kotkot@gmail.com>
2019-07-10 10:42:15 +0700

    desain detail produk

--- src/pages/detailProduct.js
+++ src/pages/detailProduct.js
@@ -4,6 +4,7 @@ import dummy from '../data/produk'
 import Navbar from '../component/header'
 import Kaki from '../component/footer'
 import Modal from '../component/modal'
+import 'bootstrap/dist/css/bootstrap.min.css'
 export default class DetailProduct extends Component {
     constructor(props) {
         super(props)
@@ -11,7 +12,7 @@ export default class DetailProduct extends Component {
         this.state = {
             data: dummy,
             cart: [],
-            qty: 0,
+            qty: 1,
         }
 
         this.add = this.add.bind(this)
@@ -41,26 +42,153 @@ export default class DetailProduct extends Component {
         let getId = Number(this.props.match.params.id_product)
         let dataById = dummy.find((item => item.id_produk === getId))
         let subTotal = dataById.price * this.state.qty
+        const flexContainer = {
+            display: "flex",
+            flexWrap: "wrap",
+        }
+
+        const flexContent = {
+            width: "20%",
+            alignItems: "center",
+            padding: "10px 0",
+            float: "left",
+            display: "flex",
+            boxSizing: "border-box"
+        }
+
+        const inlineBlock = {
+            display: "inline-block",
+            verticalAlign: "middle"
+        }
+
+        const itemContent = {
+            fontSize: "11px",
+            color: "s",
+            lineHeight: 1,
+        }
+
+        const itemValue = {
+            fontWeight: "600",
+            fontSize: "14px",
+            color: "rgba(0,0,0,.7)",
+            lineHeight: "12px",
+            paddingTop: 5
+        }
+
         return (
-            <div><Navbar />
-
+            <div class=" container">
                 <div key={dataById.id_produk} style={{ marginTop: "100px" }}>
+                    <nav aria-label="breadcrumb" style={{ fontSize: '15px' }}>
+                        <ol class="breadcrumb bg-transparent">
+                            <li class="breadcrumb-item"><a href="#">Beranda</a></li>
+                            <li class="breadcrumb-item"><a href="#">Kategori</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Data</li>
+                        </ol>
+                    </nav>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <img src={dataById.image} alt="" class="img-thumbnail" style={{ width: '380px', height: '380px' }}></img>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3 class=" font-weight-bolder">{dataById.product_name}</h3>
+                            <span>46 Ulasan</span><br></br>
+                            <div class="mt-2">
+                                <span>99.21%  Transaksi Sukses Dari  126  Transaksi </span>
+                                <button
+                                    class="btn btn-light btn-sm"
+                                    style={{ float: "right", backgroundColor: 'transparent', color: 'black', borderWidth: '2px' }} ><i class=" fa fa-share-alt fa-1x"></i>  Bagikan</button>
+                            </div>
+                            <h4 class=" font-weight-bolder mt-2" style={{ color: "#ff5722" }}>Rp. {dataById.price}</h4>
+                            <div class="mt-3">
+                                <div style={{ float: "left" }}>
+                                    <h6 class=" font-weight-bolder">Jumlah</h6>
+                                    <button
+                                        class="btn btn-primary rounded-circle btn-md text-primary"
+                                        style={{ backgroundColor: 'transparent', color: 'black' }}
+                                        disabled={this.state.qty < 2}
+                                        onClick={this.substract}>--</button>
+                                    <input type="text" class="text-center" value={this.state.qty} style={{
+                                        borderColor: 'transparent',
+                                        borderBottomWidth: 2,
+                                        borderBottomColor: 'green',
+                                        width: '30%'
+                                    }} />
+                                    <button
+                                        class="btn btn-success rounded-circle btn-md text-success"
+                                        style={{ backgroundColor: 'transparent', color: 'black' }}
+                                        onClick={this.add}>+</button>
+                                </div>
+                                <div style={{ float: "right" }}>
+                                    <h6 class=" font-weight-bolder">Catatan untuk penjual (opsional)</h6>
+                                    <input type="text" class="form-control" placeholder="Contoh: Warna Putih, Ukuran XL,Edisi Ke" />
+                                    <span class="text-secondary" style={{ float: 'right', fontSize: '13px' }}>0/144 karakter</span>
+                                </div>
+                            </div>
 
-                    <button disabled={this.state.qty < 1} onClick={this.substract}>-</button> Qty: {this.state.qty} <button onClick={this.add}>+</button>
-                    <br></br>
+                            <div style={{ float: 'left' }}>
+                                <p>Cicilan bunga 0% mulai dari Rp 2.959  <span class="text-danger font-weight-bold">Bandingkan Cicilan</span></p>
+                                <div style={flexContainer}>
+                                    <div style={flexContent}>
+                                        <i class="fa fa-eye mr-1"></i>
+                                        <div style={inlineBlock}>
+                                            <div style={itemContent}>Dilihat</div>
+                                            <div style={itemValue}>3</div>
+                                        </div>
+                                    </div>
+                                    <div style={flexContent}>
+                                        <i class="fa fa-truck mr-1"></i>
+                                        <div style={inlineBlock}>
+                                            <div style={itemContent}>Terkirim</div>
+                                            <div style={itemValue}>0</div>
+                                        </div>
+                                    </div>
+                                    <div style={flexContent}>
+                                        <i class="fa fa-box-open mr-1"></i>
+                                        <div style={inlineBlock}>
+                                            <div style={itemContent}>Kondisi</div>
+                                            <div style={itemValue}>Baru</div>
+                                        </div>
+                                    </div>
+                                    <div style={flexContent}>
+                                        <i class="fa fa-tag mr-1"></i>
+                                        <div style={inlineBlock}>
+                                            <div style={itemContent}>Min. Beli</div>
+                                            <div style={itemValue}>1</div>
+                                        </div>
+                                    </div>
+                                    <div style={flexContent}>
+                                        <i class="fa fa-shield-alt mr-1"></i>
+                                        <div style={inlineBlock}>
+                                            <div style={itemContent}>Asuransi</div>
+                                            <div style={itemValue}>Opsional</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-                    <img src={dataById.image} alt=""></img>
-                    <h1>{dataById.id_produk}. {dataById.product_name}</h1>
-                    <img src={dataById.image} alt=""></img>
-                    <h1>{dataById.id_produk}. {dataById.product_name}</h1>
-                    <img src={dataById.image} alt=""></img>
-                    <h1>{dataById.id_produk}. {dataById.product_name}</h1>
-                    <img src={dataById.image} alt=""></img>
-                    <h1>{dataById.id_produk}. {dataById.product_name}</h1>
+                            <div class="float-left mt-3">
+                                <h6>Estimasi ongkos kirim</h6>
 
-                    <Kaki kota={dataById.location} nama={dataById.seller_name} gambar={dataById.seller_foto} subTotal={subTotal} click={this.addToCart.bind(this)} />
-                    <Modal gambar={dataById.image} qty={this.state.qty} subTotal={subTotal} hargaAwal={dataById.price} />
+                                <div class="row rounded text-secondary">
+                                    <div class="col border p-3">
+                                        <p style={{ fontSize: "12px" }}>Kecamatan</p>
+                                    </div>
+                                    <div class="col border">
+                                        <p style={{ fontSize: "12px" }}>Kode Pos</p>
+                                    </div>
+                                    <div class="col border disabled">
+                                        <p style={{ fontSize: "12px" }}>Berat</p>
+                                    </div>
+                                    <div class="col text-center text-danger">
+                                        <p style={{ padding: 20, paddingLeft: 5, cursor: 'pointer' }}>Hitung</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <Kaki kota={dataById.location} nama={dataById.seller_name} gambar={dataById.seller_foto} subTotal={subTotal} click={this.addToCart.bind(this)} />
+                <Modal gambar={dataById.image} qty={this.state.qty} subTotal={subTotal} hargaAwal={dataById.price} />
             </div>
         )
     }
