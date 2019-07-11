4771f54da5b34a078d1e19b6e3160898164a8ccd
falihnaufal17 <dudu.kotkot@gmail.com>
2019-07-11 10:27:30 +0700

    you're the reason

--- src/data/cart.js
+++ src/data/cart.js
@@ -12,15 +12,6 @@ data.push(
         "image": "https://cdn.idntimes.com/content-images/post/20161208/5-35b6343f68c36d03c2495f7c84e1dede.jpg",
         "location": "jepang"
     },
-    {
-        "id_cart": 2,
-        "product_name": "LEMARI IKEA",
-        "price": 567654,
-        "qty": 1,
-        "sub_total": subTotal,
-        "image": "https://ecs7.tokopedia.net/img/cache/700/product-1/2019/1/11/4482427/4482427_016a1e90-1f8b-494b-beb9-2789890347af_700_700.jpg",
-        "location": "papua new guinea"
-    },
 )
 
 
